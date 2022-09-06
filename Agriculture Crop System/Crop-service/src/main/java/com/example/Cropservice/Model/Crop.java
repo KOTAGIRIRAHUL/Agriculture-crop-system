31c0bdade1e6e1a81dae7b861020fc825100c674
KOTAGIRIRAHUL <102713928+KOTAGIRIRAHUL@users.noreply.github.com>
2022-09-06 13:18:16 +0530

    first commit

---
+++ Agriculture Crop System/Crop-service/src/main/java/com/example/Cropservice/Model/Crop.java
@@ -0,0 +1,76 @@
+package com.example.Cropservice.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Crop {
+
+
+
+        @Id
+        private String productId;
+        private String productName;
+        private String productType;
+        private float productPrice;
+        private String location;
+        private String farmerEmail;
+
+        public String getProductId() {
+
+            return productId;
+        }
+        public void setProductId(String productId) {
+            this.productId = productId;
+        }
+        public String getProductName() {
+            return productName;
+        }
+        public void setProductName(String productName) {
+            this.productName = productName;
+        }
+        public String getProductType() {
+            return productType;
+        }
+        public void setProductType(String productType) {
+            this.productType = productType;
+        }
+        public float getProductPrice() {
+            return productPrice;
+        }
+
+        public void setProductPrice(float productPrice) {
+            this.productPrice = productPrice;
+        }
+
+        public String getLocation() {
+            return location;
+        }
+        public void setLocation(String location) {
+            this.location = location;
+        }
+
+
+        public String getFarmerEmail() {
+            return farmerEmail;
+        }
+        public void setFarmerEmail(String farmerEmail) {
+            this.farmerEmail = farmerEmail;
+        }
+        public Crop(String productId, String productName, String productType, float productPrice, String location,
+                       String farmerEmail) {
+            super();
+            this.productId = productId;
+            this.productName = productName;
+            this.productType = productType;
+            this.productPrice = productPrice;
+            this.location = location;
+            this.farmerEmail = farmerEmail;
+        }
+        public Crop() {
+            super();
+        }
+
+
+
+    }
